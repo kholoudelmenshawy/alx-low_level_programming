863f0828b88cf3b1768a15c45dd07c6468853c5f
kholoudelmenshawy <khlouod.khaled1996@gmail.com>
2023-09-07 18:43:28 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
- * Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
- * 
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
   {
