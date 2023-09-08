e1a60d0a9d2317de35ec53ddb8d49403206795f4
kholoudelmenshawy <khlouod.khaled1996@gmail.com>
2023-09-07 20:00:19 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{printf("Size of a char:%d byte(s),Size of an int:%d byte(s),Size of a long int:%dbyte(s),Size of a long long int:%d byte(s),Size of a float:%d byte(s)"c,i,l,ll,f);
+return (0);
+}
