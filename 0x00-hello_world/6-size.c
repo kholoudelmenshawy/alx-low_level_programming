5dd30417d6b6b2b137cd76ad4576b619494f8e3d
kholoudelmenshawy <khlouod.khaled1996@gmail.com>
2023-09-07 20:03:54 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,6 +4,6 @@
  * Return: 0 (Success)
  */
 int main(void)
-{printf("Size of a char:%d byte(s),Size of an int:%d byte(s),Size of a long int:%dbyte(s),Size of a long long int:%d byte(s),Size of a float:%d byte(s)"c,i,l,ll,f);
+{printf("Size of a char:%d byte(s),Size of an int:%d byte(s),Size of a long int:%dbyte(s),Size of a long long int:%d byte(s),Size of a float:%d byte(s)",c,i,l,ll,f);
 return (0);
 }
